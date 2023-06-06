166909766ce866e6c095b9a1a742dcf1b00ae965
joyful-young <isglory22@gmail.com>
2023-06-06 17:35:28 +0900

    카운터 컴포넌트 구성

---
+++ project1/src/components/Controller.jsx
@@ -0,0 +1,14 @@
+const Controller = () => {
+  return (
+    <>
+      <button>-1</button>
+      <button>-10</button>
+      <button>-100</button>
+      <button>+100</button>
+      <button>+10</button>
+      <button>+1</button>
+    </>
+  )
+};
+
+export default Controller;
