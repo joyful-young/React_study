b1294dedeb1075ddfb423ac79e085a59d20262e0
joyful-young <isglory22@gmail.com>
2023-06-06 17:34:10 +0900

    chapter 5

--- react-study/src/components/Body.jsx
+++ react-study/src/components/Body.jsx
@@ -1,10 +1,30 @@
 // import React from "react";
-import "./Body.css";
+import { useRef, useState } from "react";
 
+const Body = () => {
+  const [text, setText] = useState("")
+  const textRef = useRef()
 
-const Body = ({ children }) => {
-  console.log(children)
-  return <div>{children}</div>
-}
+  const handleOnChange = (e) => {
+    setText(e.target.value)
+  }
 
+  const handleOnClick = () => {
+    console.log(textRef.current.value)
+    console.log(textRef.current)
+    if (text.length < 5) {
+      textRef.current.focus()
+    } else {
+      alert(text)
+      setText("")
+    }
+  }
+
+  return (
+    <div>
+      <input ref={textRef} type="text" value={text} onChange={handleOnChange} />
+      <button onClick={handleOnClick}>작성 완료</button>
+    </div>
+  )
+};
 export default Body;
