91089eaae01dc62de8b25249999b5d3a97414ef5
joyful-young <isglory22@gmail.com>
2023-06-08 17:38:46 +0900

    230608 리액트 데이터 전달 특징, 라이프 사이클

--- project1/README.md
+++ project1/README.md
@@ -18,3 +18,21 @@
 ### 리액트 앱 만들기
 
 - `$ yarn create vite project1 --template react`
+
+### Vue로 만들어보자
+
+- `project1-vue`
+- count 변수를 app.vue에 정의
+- Controller 컴포넌트에서 버튼을 누르면 emit으로 버튼의 값을 app.vue에 전달
+- 버튼의 값에 따라 count 변수 조절해서 Viewer 컴포넌트에 props로 내려줌
+- Viewer 컴포넌트에서 interpolation으로 출력
+
+<br/>
+
+- props & emit으로 데이터 전달하는 건 부모-자식 컴포넌트 사이여야 함
+- controller와 viewer 사이 직접 전달은 안 됨
+- count를 두 컴포넌트의 부모 컴포넌트인 app에 정의해놓고 controller에서 발생한 이벤트에 따라 값을 바꿔서 viewer로 전달하는 게 좋겠다고 판단
+
+### State Lifting (State 끌어올리기)
+
+- 리액트는 State 값이나 set 함수를 여러 컴포넌트에서 사용하는 경우, 이들을 상위 컴포넌트에서 관리
--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,17 +1,46 @@
 import './App.css'
+import { useRef, useEffect, useState } from 'react'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
 
 function App() {
+  const didMountRef = useRef(false)
+  // count, setCount 정의. count 초기값은 0
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+  const handleSetCount = (value) => {
+    setCount(count + value)
+  }
+  const handleChangeText = (e) => {
+    setText(e.target.value)
+  }
+
+  // useEffect(callback, [deps])
+  // [deps]: 의존성 배열. Dependency Array
+  // useEffect는 의존성 배열 요소의 값이 변경되면 첫 번째 인수로 전달한 콜백 함수를 실행함
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true
+    } else {
+      console.log("컴포넌트 업데이트")
+    }
+  })
+
+  useEffect(() => {
+    console.log("컴포넌트 마운트")
+  }, [])
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <Viewer />
+        <input type="text" value={text} onChange={handleChangeText} />
       </section>
       <section>
-        <Controller />
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount} />
       </section>
     </div>
   )
